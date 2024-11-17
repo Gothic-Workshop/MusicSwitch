691c85d1e175fa14d1201bc03bb211853c9c9990
Arkhan <49446701+DamianQualshy@users.noreply.github.com>
2024-11-17 20:02:50 +0100

    Init Commit
    with example script provided by D36 (thank you)

---
+++ SYSTEM/AUTORUN/MusicChange.d
@@ -0,0 +1,13 @@
+META
+{
+    Parser = MUSIC;
+};
+
+test(XAR_DAY_STD)
+{
+    instance XAR_DAY_STD(C_MUSICTHEME_DEF)
+    {
+        XAR_DAY_STD_Old();
+        file = "cam_day_std.sgt";
+    };
+};
